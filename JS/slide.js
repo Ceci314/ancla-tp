a58df2dc303c68c59bf2540e36872bd654cdba97
Cecilia Palacios <celau314@gmail.com>
2023-05-21 12:28:20 -0300

    arreglo js del slide

---
+++ JS/slide.js
@@ -0,0 +1,20 @@
+var cad3= `<div class="slider-frame">
+<ul>
+    <li>
+        <div class="sld1">
+        <p>INSCRIPCION ABIERTA 2023 - 2do semestre<br>Capacitaciones Cortas y continuas</p>
+        </div>
+    </li>
+    <li> <div class="sld2">
+        <p>Cursos presenciales en nuestro Instituto <br> o Virtuales 100% a distancia!!!!
+        </p>
+    </div>
+    </li>
+    <li><div class="sld3">
+        <p>Nuestras Áreas de Formación son:<br> LABORATORIO - IMAGENES - ENFERMERIA - ADMINISTRACIÓN HOSPITALARIA - CLINICA ASISTENCIAL</p>
+    </div>
+    </li>
+</ul>
+</div>`
+
+document.getElementById("slider").innerHTML = cad3;
